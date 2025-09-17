043f26a14e332e0270c76c279fe9c90b3639097b
Varun Singh Tanwar <tanwarvarunsingh8@gmail.com>
2025-09-17 19:13:16 +0530

    update

--- script.js
+++ script.js
@@ -21,8 +21,32 @@ const exploreBtn = document.getElementById("exploreBtn");
 const productsSection = document.getElementById("products");
 
 exploreBtn.addEventListener("click", () => {
+    const offset = 50; // gap from top
     window.scrollTo({
-        top: productsSection.offsetTop, // scroll from top
-        behavior: "smooth"             // smooth scrolling
+        top: productsSection.offsetTop - offset, // scroll with gap
+        behavior: "smooth"
+    });
+});
+
+
+
+// Scroll to top button
+
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+// Show button after scrolling down
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+        scrollTopBtn.style.display = "block";
+    } else {
+        scrollTopBtn.style.display = "none";
+    }
+});
+
+// Scroll back to top smoothly
+scrollTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
     });
 });
